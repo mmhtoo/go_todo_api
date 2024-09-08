1394eab5bb97999d81051f537d7964ee22688279
mmhtoo <mmhtoo.dev@gmail.com>
2024-09-08 17:04:38 +0630

    feat: base hello world api

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mmhtoo/go-todo-api
 
 go 1.21.1
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func loadPortFromEnv() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+	return port
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	// load env file
+	godotenv.Load(".env")
+	// create new router
+	router := chi.NewRouter()
+
+	// get port from env
+	port := loadPortFromEnv()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+	
+	server := &http.Server{
+		Handler: router,
+		Addr: ":"+port,
+	}
+
+	fmt.Printf("Server is listening on port %s \n",port)
+	serverError := server.ListenAndServe()
+
+	if serverError != nil {
+		fmt.Printf("Error: %s", serverError)
+	}
+
 }
